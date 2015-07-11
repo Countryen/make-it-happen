afb83046670b370474ccb4cc90dbe2bb3f42b157
Countryen <countryen@kabelbw.de>
2015-07-11 12:08:17 +0200

    1. added index.js as controller for index.html; 2.changed the App to Author.App (global namespace Author); 3. Added functionality for loading a file from filesystem to the menu

--- Authors/BrowserAuthor/BrowserAuthor/app.js
+++ Authors/BrowserAuthor/BrowserAuthor/app.js
@@ -1,46 +1,53 @@
 ﻿/*
-    Project: Make It Happen (Toolset for Generation of choice - and - consequence games)
-    Subproject: Author(Tool / App) - Writer(Person) uses the Author to create the game (*.book)
-    Authors: Countryen
-    This Project is Open Source, see the LICENCE - File for more information.
-    https://github.com/countryen/make-it-happen
+Project: Make It Happen (Toolset for Generation of choice - and - consequence games)
+Subproject: Author(Tool / App) - Writer(Person) uses the Author to create the game (*.book)
+Authors: Countryen
+This Project is Open Source, see the LICENCE - File for more information.
+https://github.com/countryen/make-it-happen
 
-    File-Info:
-    app.js - > The Application itself. Main Class for several usefull meta-methods.
-              Should be created (instance) on appstart.
-    Countryen, 8th July 2015 @ C0 | VS -Villingen, Germany.
+File-Info:
+app.js - > The Application itself. Main Class for several usefull meta-methods.
+          Should be created (instance) on appstart.
+Countryen, 8th July 2015 @ C0 | VS -Villingen, Germany.
 */
 /**
  * Main (general) class for the whole app. Should contain everything needed by the devs involving the environment etc.
 */
-function App() {
-    this.elements = {
-        menuExitButton: { id: "menu-btn-exit" }
-    };
+(function () {
+    window.Author = window.Author || {};
 
-    // closes the app / window
-    this.exit = function() {
-        window.close();
-    }
-    
-    // helper-functions for global use
-    this.helper = {
-        // like [].indexOf.call() -> C#
-        contains: function (container, item) {
-            if (([].indexOf.call(container, item) != -1) || ((container.indexOf) && (container.indexOf(item) != -1)))
-                return true;
-            else
-                return false;
+    window.Author.App = function App() {
+        this.elements = {
+            menuExitButton: { id: "menu-btn-exit" },
+            menuLoadButton: { id: "menu-btn-load" },
+            menuLoadInput: { id: "menu-input-load" }
+        };
+
+        // closes the app / window
+        this.exit = function () {
+            window.close();
+        }
+
+        // helper-functions for global use
+        this.helper = {
+            // like [].indexOf.call() -> C#
+            contains: function (container, item) {
+                if (([].indexOf.call(container, item) != -1) || ((container.indexOf) && (container.indexOf(item) != -1)))
+                    return true;
+                else
+                    return false;
+            },
+            // like document.getElementById / .getElementsByClassName / getElementsByTagName
+            get: function (selector) {
+                if (this.contains(selector, "#"))
+                    return document.getElementById(selector.replace("#", ""));
+                else if (this.contains(selector, "."))
+                    return document.getElementsByClassName(selector.replace(".", ""));
+                else
+                    return document.getElementsByTagName(selector);
+            }
+
         }
     }
-}
 
-// On start -> Loads the app and registers all events etc...
-window.onload = function() {
-    var app = new App();
-    var exitButton = document.getElementById(app.elements.menuExitButton.id);
-    exitButton.onclick = function () {
-    //app.exit();
-}
-
-};
+})();
---
+++ Authors/BrowserAuthor/BrowserAuthor/index.js
@@ -0,0 +1,56 @@
+﻿/*
+    Project: Make It Happen (Toolset for Generation of choice-and-consequence games)
+    Subproject: Author (Tool/App) - Writer (Person) uses the Author to create the game (*.book)
+    Authors: Countryen
+    This Project is Open Source, see the LICENCE-File for more information.
+    https://github.com/countryen/make-it-happen
+
+    File-Info:
+    index.js -> Controller for index.html (the menu) -> registering events etc.
+    Countryen, 11th July 2015 @ C0 | VS-Villingen.
+*/
+(function () {
+    // Global app-class.
+    var app = new Author.App();
+
+    // menu-btn-exit -> OnClick -> Exit the app
+    window.addEventListener("load", function () {
+        var exitButton = document.getElementById(app.elements.menuExitButton.id);
+        exitButton.onclick = function () {
+            app.exit();
+        }
+    });
+
+    // menu-btn-load -> OnClick -> Load the selected book
+    // Help by: http://www.htmlgoodies.com/beyond/javascript/read-text-files-using-the-javascript-filereader.html#fbid=cv2OE3ULtAv
+    // The file MUST be utf-8!
+    window.addEventListener("load", function () {
+        var loadButton = document.getElementById(app.elements.menuLoadButton.id);
+        loadButton.onclick = function () {
+            // HTMLInputElement = https://developer.mozilla.org/de/docs/Web/API/HTMLInputElement
+            var loadInput = document.getElementById(app.elements.menuLoadInput.id);
+
+            var selectedFiles = loadInput.files;
+            if (selectedFiles.length !== 1)
+                throw Error("No file selected, loading can not proceed");
+            
+            // Create a new book and load from file.
+            // The file must be utf-8!
+            
+            var selectedFile = selectedFiles[0];
+            var fileReader = new FileReader();
+
+            // Asynchronous reading. The file must be utf-8!
+            fileReader.onload = fileReader_OnLoad; // see below.
+            fileReader.readAsText(selectedFile);
+
+            function fileReader_OnLoad(fileReaderEvent) {
+                var content = fileReaderEvent.target.result;
+                console.log(content);
+            }
+            
+            // TODO: -> new Book()~.loadFromFile(file);
+        }
+    });
+
+})();
